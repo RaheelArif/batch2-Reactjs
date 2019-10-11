671e16275bf375bdc9028bfc71598b78e3216615
raheelarif <raheelarif>
2019-10-11 18:00:36 +0500

    11/10/2019

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -4,7 +4,8 @@ import LeftSide from "./leftSide";
 class Container extends Component {
 state = {
     name : null,
-    price : null
+    price : null,
+    data : null,
 }
 
 handleChange = (e) => {
@@ -12,11 +13,15 @@ this.setState ({
     [e.target.name] : e.target.value
 })
 }
-
+getData = (data) => {
+      this.setState({
+          data : data
+      })  
+}
 
 
 render() {
-
+console.log(this.state)
         return (
             <div>
                 <h1>{this.state.name}</h1>
@@ -24,7 +29,7 @@ render() {
                 <h1>{this.state.price}</h1>
                 <input onChange={this.handleChange} placeholder="price" name="price" type="text"/>
 
-                < LeftSide />
+                < LeftSide  getData = {(data) => this.getData(data)}/>
 
             </div>
         )
--- src/components/container/leftSide.js
+++ src/components/container/leftSide.js
@@ -1,37 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
-class LeftSide extends Component{
+class LeftSide extends Component {
     state = {
-        
-        Products : [
-            {name: "shugar",price: "20Rs" },
-            {name: "oil",price: "40Rs" },
-            {name: "perfume",price: "50Rs" },
-            {name: "shugar1",price: "60Rs" },
-            {name: "ink",price: "23Rs" },
-            {name: "pen",price: "70Rs" },
+
+        Products: [
+            { name: "shugar", price: "20Rs" },
+            { name: "oil", price: "40Rs" },
+            { name: "perfume", price: "50Rs" },
+            { name: "shugar1", price: "60Rs" },
+            { name: "ink", price: "23Rs" },
+            { name: "pen", price: "70Rs" },
 
         ]
     }
-    
-    render(){
-            // var sortedProducts = this.state.Products.filter( item => {
-            //     return item.price === "40Rs";
-            // })
+    deletItem = (e) => {
+      
+       this.setState({
+           Products : this.state.Products.filter( product => { return product.name != e.target.id })
+       })
+    }
+sendData = () => {
+
+    this.props.getData(this.state)
+}
+    render() {
 
 
-        var data =this.state.Products.map( product =>{
-            return  <h1 key={product.name}>Product Name : {product.name} || Product Price : {product.price}</h1>
+        var data = this.state.Products.map(product => {
+            return (
+                <tr key={product.name}>
+                   <td>{product.name}</td>
+                   <td>{product.price}</td>
+                   <td onClick={this.deletItem} id={product.name} style={{color : "red"}}>X</td>
 
-          } )
+                </tr>
+            )
 
-        return(
+        })
+
+        return (
             <div>
+                <table border="1">
+                    <tr>
+                        <th>Product name </th>
+                        <th>Product Price </th>
+                        <th>delet item</th>
+                    </tr>
+                {data}
+                </table>
 
-                  {data}
-
-              
+<button onClick={this.sendData}>send data</button>
             </div>
         )
     }
